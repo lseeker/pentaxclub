7d3d0f03b7f91b8d4bab00f634362c2d7a4e7400
Yun-young LEE <progcom@gmail.com>
2016-06-30 05:52:19 +0900

    initial testing...

--- src/pentaxclub.js
+++ src/pentaxclub.js
@@ -3,9 +3,12 @@
 require('marko/node-require').install();
 
 const koa = require('koa');
+const router = require('koa-router')();
+const serve = require('koa-static');
+
 const app = koa();
 
-app.use(function *() {
+router.get('/', function*() {
 	var template = require('./pages/main.marko');
 
 	this.type = 'html'
@@ -13,6 +16,11 @@ app.use(function *() {
                 site: { title: 'Pentaxclub beta' }
 	});
 });
+
+
+app.use(serve('./public'));
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
 
