7bc940d7c8ea84749a9dab2066c11b3e2dc3fad6
Yun-young LEE <progcom@gmail.com>
2016-07-04 19:01:10 +0900

    complete navbar

--- src/public/pentaxclub.js
+++ src/public/pentaxclub.js
@@ -1,13 +1,14 @@
 var pentaxRed = '#d7003a';
 
 $(document).ready(function() {
+    // on collapsed
     $('#navbtn').css('cursor', 'pointer')
     .hover(function() {
         $('a', this).css('color', pentaxRed);
     }, function() {
         $('a', this).css('color', 'inherit');
     }).click(function() {
-        $('#navbtn + ul').slideToggle(100);
+        $('#navbtn + ul').stop().slideToggle(100);
         return false;
     });
 
@@ -16,21 +17,51 @@ $(document).ready(function() {
         if ($('ul', this).is(':visible')) {
             return;
         }
+
         $(this).css('background-color', '#f0f0f0');
+        if (!$('#navbtn').is(':visible')) {
+            $('> a', this).css('color', pentaxRed);
+        }
     }, function() {
         $(this).css('background-color', 'inherit');
         $('> a', this).css('color', 'inherit');
     }).click(function() {
         if ($(this).has('ul').length) {
-            if ($('ul', this).is(':visible')) {
-                $(this).css('background-color', '#f0f0f0').css('padding-bottom', '.4rem');
+            if ($('#navbtn').is(':visible')) {
+                if ($('ul', this).is(':visible')) {
+                    $(this).css('background-color', '#f0f0f0').css('padding-bottom', '.4rem');
+                } else {
+                    $(this).css('background-color', 'inherit').css('padding-bottom', '0');
+                }
+                $('ul', this).stop().slideToggle(100);
             } else {
-                $(this).css('background-color', 'inherit').css('padding-bottom', '0');
+                if ($('ul', this).is(':visible')) {
+                    $(this).css('background-color', '#f0f0f0');
+                    $('ul', this).stop().slideUp(100);
+                } else {
+                    $(this).css('background-color', 'inherit');
+
+                    // close other ul
+                    $('nav li ul').stop().hide();
+                    $('ul', this).stop().fadeToggle(100);
+                }
             }
-            $('ul', this).slideToggle(100);
         } else {
             location.href = $('a', this).attr('href');
         }
         return false;
+    });
+
+    $('nav > ul > li').css('cursor', 'pointer').mouseover(function() {
+        if ($('ul', this).is(':visible')) {
+            return;
+        }
+        if ($('#navbtn').is(':visible')) {
+            return;
+        }
+
+        // close other ul
+        $('nav li ul').stop().hide();
+        $('ul', this).stop().fadeIn(100);
     });
 });
